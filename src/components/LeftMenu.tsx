e6bd0c80a5f6d55a942b9daf6431f6399f9c99a1
Doru Furtuna <doru.furtuna@gmail.com>
2018-12-20 10:08:36 +0200

    Added logo and newChart button.

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,13 +1,24 @@
 import * as React from "react";
 import "./LeftMenu.css";
 
-import { faChartBar, faHistory, faHome, faUserFriends } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChartBar,
+  faHistory,
+  faHome,
+  faUserFriends,
+  faPlus
+} from "@fortawesome/free-solid-svg-icons";
 import { MenuLink } from "./MenuLink";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class LeftMenu extends React.Component {
   public render() {
     return (
       <div className="LeftMenuWrapper">
+        <div className="Logo">react-d3</div>
+        <button className="AddButton">
+          <FontAwesomeIcon icon={faPlus} className="ButtonIcon"/> Create new Chart
+        </button>
         <MenuLink to="/dashboard" label="Dashboard" icon={faHome} />
         <MenuLink to="/transactions" label="Transactions" icon={faChartBar} />
         <MenuLink to="/accounts" label="Accounts" icon={faUserFriends} />
